b21abbc90299adfa17a48245c27f039ef59cbe5d
neumann1337 <bartlomiejneumann@proton.me>
2026-04-21 12:10:48 +0200

    update

--- src/components/bonds/BondCard.tsx
+++ src/components/bonds/BondCard.tsx
@@ -114,7 +114,7 @@ export default function BondDetails() {
                 </header>
 
                 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 mb-24">
-                {bondsToDisplay.map((bond) => (
+                {bondsToDisplay.map((§) => (
                     <BondCard key={bond.symbol} bond={bond} />
                 ))}
                 </section>
